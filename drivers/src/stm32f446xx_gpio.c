4de79a270131696aad5323f033f929bf2308cdc2
Gopal Agarwal <44130448+Gopalagarwal040@users.noreply.github.com>
2021-09-14 13:42:33 +0530

    Update stm32f446xx_gpio.c

--- drivers/src/stm32f446xx_gpio.c
+++ drivers/src/stm32f446xx_gpio.c
@@ -2,7 +2,7 @@
  * stm32f446xx_gpio.c
  *
  *  Created on: Apr 8, 2021
- *      Author: Apoorv singh negi
+ *      Author: Gopal Agarwal
  */
 #include "stm32f446xx.h"
 #include "stm32f446xx_gpio.h"
